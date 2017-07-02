d69bd422b2ef0e4d4292407dc3c0f391a7bfbba2
李白 <ziyiqi1219@qq.com>
2017-07-02 18:07:07 +0800

    创建结构目录，配置Home组件路由

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # React-Demo
+##react技术栈
+> react react-router-dom redux react-redux
+
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+let path=require('path');
+let HtmlWebpackPlugin=require('html-webpack-plugin');
+module.exports={
+    entry:'./app/index.js',
+    output: {
+        filename: "bundle.js",
+        path:path.resolve('dist')
+    },
+    module: {
+        rules:[
+            {
+                test:/\.js$/,
+                use:'babel-loader',
+                exclude:/node_modules/
+            },
+            {
+                test:/\.less/,
+                use:['style-loader','css-loader','less-loader']
+
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template:'./app/index.html'
+        })
+    ],
+    devtool:'source-map',
+    devServer: {
+        proxy:{'/api':'http://localhost:3000'}
+    }
+};
